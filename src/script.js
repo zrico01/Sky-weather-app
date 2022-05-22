fd6ad91deb45f8a576b7a2551228012b5e7f48ec
zrico01 <102926038+zrico01@users.noreply.github.com>
2022-05-21 19:01:43 -0700

    Update function
    showWeatherCondition function update

--- src/script.js
+++ src/script.js
@@ -46,17 +46,16 @@ h3.innerHTML = `${currentDay} ${currentMonth} ${date}, ${year} ${hour}:${minutes
 //Search engine
 
 function showWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-
-  document.querySelector("#humidityMarker").innerHTML =
-    response.data.main.humidity;
-  document.querySelector("#windMarker").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#mainDes").innerHTML = response.data.weather[0].main;
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 function search(city) {
@@ -65,7 +64,7 @@ function search(city) {
   let apiEndpoint = "htttps://api.openweathermap.org/data/2.5/weather";
 
   let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
-  console.log(axios);
+  console.log(apiUrl);
   axios.get(apiUrl).then(showWeatherCondition);
 }
 
